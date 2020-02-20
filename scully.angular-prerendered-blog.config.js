98856c4b20fddc93bcd8c15d53ef708e776ef4a4
Tara Z. Manicsic <tzmanics@gmail.com>
2020-02-20 07:27:43 -0500

    generates blog via scully

--- scully.angular-prerendered-blog.config.js
+++ scully.angular-prerendered-blog.config.js
@@ -3,5 +3,11 @@ exports.config = {
   projectName: "angular-prerendered-blog",
   outDir: './dist/static',
   routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
   }
 };
